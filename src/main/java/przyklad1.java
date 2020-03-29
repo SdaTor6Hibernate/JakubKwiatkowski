b02f713040ec3ced49d0b219cd803fc2aa30e228
kwiatas83 <kwiatas83@gmail.com>
2020-03-29 11:16:30 +0200

    mapping

--- src/main/java/przyklad1.java
+++ src/main/java/przyklad1.java
@@ -1,2 +1,17 @@
+import model.Country;
+import model.Order;
+import model.User;
+import org.hibernate.Session;
+import util.HibernateUtil;
+
 public class przyklad1 {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Order print = session.find(Order.class, 1);
+        session.close();
+        System.out.println(print.toString());
+    }
+
+
 }
